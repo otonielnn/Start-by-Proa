233616d307993249fb8484cd8d851e7038dfcf5d
otonielnn <otoniel_2002@live.com>
2024-12-03 13:45:02 -0300

    Criando uma função de multiplicação e seu teste
    Signed-off-by: otonielnn <otoniel_2002@live.com>

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 def soma(a, b):
     return a + b
 
+def multiplicacao(a, b):
+    return a * b
+
 if __name__ == "__main__":
     print(f"A soma de 2 e 3 é: {soma(2, 3)}")
+    print(f"A multiplicação de 2 e 3 é: {multiplicacao(2, 3)}")
--- test_app.py
+++ test_app.py
@@ -1,4 +1,13 @@
-from app import soma
+from app import *
 
 def test_soma():
     assert soma(2, 3) == 5
+
+def test_multiplicacao():
+    # arrange
+    numero1 = 2
+    numero2 = 3
+    # act
+    resultado = multiplicacao(numero1, numero2)
+    # assert
+    assert resultado == 6
